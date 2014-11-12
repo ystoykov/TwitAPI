8d3ee6c188b87fc0928d60fad98b0758e554117c
MRTOdessa <MRTOdessa@192.168.0.100>
2014-11-12 17:36:40 +0200

    with interface

---
+++ src/twitteroperation/TwitterClient.java
@@ -0,0 +1,12 @@
+package twitteroperation;
+
+public interface TwitterClient {
+	
+	public  void postingStatus(String post);
+	
+	public getingStatus(String username);
+	
+	public getingUserTimeline(String username);
+	
+
+}
