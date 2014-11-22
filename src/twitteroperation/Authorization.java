f50aec2f743bb31675d96fa7c0897143f840bf6a
ystoykov <stoykovyakov@gmail.com>
2014-11-22 17:11:41 +0200

    create config file

--- src/twitteroperation/Authorization.java
+++ src/twitteroperation/Authorization.java
@@ -1,6 +1,24 @@
 package twitteroperation;
 
+import twitter4j.conf.ConfigurationBuilder;
+
 public class Authorization {
 	
+	String CONSUMER_KEY;
+	String CONSUMER_SECRET;
+	String TOKEN;
+	String TOKEN_SECRET;
+	
+	Authorization () throws Exception{
+		
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		
+		cb.setDebugEnabled(true)
+			.setOAuthConsumerKey(CONSUMER_KEY)
+			.setOAuthConsumerSecret(CONSUMER_SECRET)
+			.setOAuthAccessToken(TOKEN)
+			.setOAuthAccessTokenSecret(TOKEN_SECRET);
+		return;
+	}
 
 }
